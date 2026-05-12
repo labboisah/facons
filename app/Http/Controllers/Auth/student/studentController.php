fab1c3b60744be3ae4943a91c055409dd7c7d741
akibubello141 <akibubello14@gmail.com>
2026-05-12 10:13:53 +0100

    studentController created

---
+++ app/Http/Controllers/Auth/student/studentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\auth\student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class studentController extends Controller
+{
+    //
+}
